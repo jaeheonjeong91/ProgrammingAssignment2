a3d922dced4a7b00af5ecf7c7772947ff95ebaeb
jaeheonjeong <jaeheonjeong91@gmail.com>
2017-08-20 19:53:40 +0900

    proto_ver
    need more test

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-## AA
+## Caching the Mean of a Vector
+## Info: Created based on Assignment Example and Verified the numbers through Web-based Inverser Matrix Calculator
 
-## Write a short comment describing this function
+## Basically it cache matrix into the repository
 
 makeCacheMatrix <- function(x = matrix()) {
+  
+  inv <- NULL
+  set <- function(y) {
+      x <<- y
+      inv <<- NULL
+  }
 
+  get <- function() x
+  setinverse <- function(inverse) inv
+  getinverse <- function() inv
+  list(set = set, 
+       get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Return inverse matrix of x
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinverse()
+  if(!is.null(inv)) {
+            message("getting cached data")
+            return(inv)
+  }
+  data <- x$get()
+  inv <- solve(mat, ...)
+  x$setinverse(inv)
+  inv
+       
 }
